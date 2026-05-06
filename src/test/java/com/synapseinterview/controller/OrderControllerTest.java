4583a455ad6d7a0fcce40d1fe9631145d8d0e937
andrew barbanera <andrewbarbanera@gmail.com>
2026-05-06 15:54:55 -0600

    test: import GlobalExceptionHandler in OrderControllerTest for accurate web layer context

--- src/test/java/com/synapseinterview/controller/OrderControllerTest.java
+++ src/test/java/com/synapseinterview/controller/OrderControllerTest.java
@@ -6,6 +6,7 @@ import com.synapseinterview.service.OrderRoutingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(OrderController.class)
+@Import(GlobalExceptionHandler.class)
 class OrderControllerTest {
 
     @Autowired MockMvc mockMvc;
